426873fdf29a56192dcde6ac1dbca3dfe756ba5b
gustavo ruan <gustavo@mail.com>
2026-05-21 15:30:56 -0300

    commit 21/05/26.1

--- Cachorro.java
+++ Cachorro.java
@@ -1,5 +1,26 @@
 package vet;
 
 public class Cachorro {
+	String nome;
+	String raca;
+	int idade;
+	boolean faminto = true;
+
+	public void comer() {
+		if (faminto == true) {
+			System.out.println("O " + nome + " comeu!");
+		} else {
+			System.out.println(nome + "já está satisfeito.");
+		}
+	}
+
+	public void latir() {
+		System.out.println("O " + raca + " chamado " + nome + " diz \"AU AU!\"");
+	}
+	
+	public void exibirDados() {
+		System.out.println("Dados do cachorro:\n Nome:" + nome + "\nRaça: " + raca + "\nIdade: " + idade + " anos"); 
+	}
 
 }
+
--- ClinicaVeterinaria.java
+++ ClinicaVeterinaria.java
@@ -1,5 +1,26 @@
 package vet;
 
 public class ClinicaVeterinaria {
-
+	public static void main (String [] args) {
+		Cachorro Lili = new Cachorro();
+		Cachorro Amora = new Cachorro();
+		
+		Lili.nome = "Lili";
+		Lili.raca = "SRD";
+		Lili.idade = 13;
+		Lili.faminto = true;
+		
+		Amora.nome = "Amora";
+		Amora.raca = "SRD"; 
+		Amora.idade = 11;
+		Amora.faminto = false;
+		
+		Lili.exibirDados(); 
+		Amora.exibirDados();
+		
+		Lili.comer();
+		Amora.latir();
+		
+	}
 }
+
